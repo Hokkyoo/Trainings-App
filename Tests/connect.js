438df4828ac43b7d982d6ef73aa1be1d17d3cce8
Dennis <78407546+Hokkyoo@users.noreply.github.com>
2021-05-11 08:39:55 +0200

    Create connect.js

---
+++ Tests/connect.js
@@ -0,0 +1,23 @@
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
+var TYPES = require('tedious').TYPES;
+
+// Create connection to database
+var config = {
+  userName: 'adminchannel', 
+  password: 'Moinbinderben',
+  server: 'localhost',
+  options: {
+      database: 'SampleDB'
+  }
+}
+var connection = new Connection(config);
+
+// Attempt to connect and execute queries if connection goes through
+connection.on('connect', function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Connected');
+  }
+});
